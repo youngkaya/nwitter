8a242acfb7dca539d74adb379d8e46ef1c7db376
youngkaya <iamjaehyun4@gmail.com>
2022-05-12 05:32:53 +0900

    1.6 Log In system

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -4,7 +4,8 @@ import React, { useState } from "react";
 const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [newAccount, setNewAccount] = useState(false);
+    const [newAccount, setNewAccount] = useState(true);
+    const [error, setError] = useState("");
     const onChange = (event) => {
         const {
             target: { name, value },
@@ -26,9 +27,11 @@ const Auth = () => {
             }
             console.log(data);
         } catch (error) {
-            console.log(error);
+            setError(error.message);
         }
     };
+    const toggleAccount = () => setNewAccount((prev) => !prev);
+
     return (
         <div>
             <form onSubmit={onSubmit}>
@@ -48,8 +51,15 @@ const Auth = () => {
                     value={password}
                     onChange={onChange}
                 />
-                <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+                <input
+                    type="submit"
+                    value={newAccount ? "Create Account" : "Log In"}
+                />
+                {error}
             </form>
+            <span onClick={toggleAccount}>
+                {newAccount ? "Sign In" : "Create Account"}
+            </span>
             <div>
                 <button>Continue with Google</button>
                 <button>Continue with Github</button>
